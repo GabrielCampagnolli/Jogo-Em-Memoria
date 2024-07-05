e35fb5c7ff4f6cd958cc32659e93611a46777dcb
Gabriel Campagnolli <gabrielcampagnolli999@gmail.com>
2024-07-05 16:36:06 -0300

    Add files via upload

---
+++ src/joguinhomuitoloco/Joguinhomuitoloco.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package joguinhomuitoloco;
+
+/**
+ *
+ * @author gabri
+ */
+public class Joguinhomuitoloco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaInicial tela1=new TelaInicial();
+        tela1.setVisible(true);
+    }
+    
+}
